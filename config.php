4b12815b82539493ed8a17f4032fd0ed65025435
sha-in21 <shain.sivetcsb@gmail.com>
2026-06-15 21:57:48 +0530

    Removed database password from config

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "Alhamdhulill@h21!", "internship_tracker");
+$conn = new mysqli("localhost", "root", "", "internship_tracker");
 if($conn->connect_error){
     die("Database Connection Failed: " . $conn->connect_error);
 }
